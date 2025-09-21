f9740fcc676d41742a3bdadf587751f8468e086d
Royal-dudy99 <royaldudy99@gmail.com>
2025-09-21 19:53:32 +0530

    fix: correct extends to plugin:@typescript-eslint/recommended

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,21 +2,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended", // <- the only valid way for v6+
+    "plugin:react-hooks/recommended"
   ],
-  ignorePatterns: ['dist', '.eslintrc.js'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.js"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "react-refresh"
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true }
     ],
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'prefer-const': 'error',
-    'no-var': 'error',
-  },
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prefer-const": "error",
+    "no-var": "error"
+  }
 }
